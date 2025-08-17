fbeef78734d851594f9a0420054c8e6c50350359
Deposer <gayfindme@yandex.com>
2025-08-17 23:45:55 +0530

    Initial commit

--- otpbot@moveon/models/allowed.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const allowedSchema = new Schema({
-  telegram_id: { type: String },
-});
-
-module.exports = mongoose.model("Allowed", allowedSchema);
